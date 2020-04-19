bc5aaae384b3b5698f0a4d52cc5f8ecad84c6703
Banee Ishaque K <baneeishaque@gmail.com>
2020-04-19 17:45:00 +0530

    Create adb_wi-fi_with_scrcpy.cmd

---
+++ adb_wi-fi_with_scrcpy.cmd
@@ -0,0 +1,36 @@
+@Echo Off 
+
+Set choice=H
+Set /P choice=External Network(press N) or Device Hotspot(default - just press Enter) : 
+
+Set adb_kill=N
+Set /P adb_kill=Kill adb (Y/N - default is No) : 
+if "%adb_kill%"=="Y" (
+
+    adb kill-server
+    
+ )
+
+adb tcpip 5555
+
+If "%choice%"=="N" (
+
+    Set /P ip=Enter Device IP : 
+
+ ) Else (
+ 
+    adb connect 192.168.43.1:5555
+    REM adb -s 192.168.43.1:5555 usb
+    
+ )
+
+If "%choice%"=="N" (
+ 
+    :: Echo %ip%
+    adb connect %ip%:5555
+    REM adb -s %ip%:5555 usb
+    
+ )
+ 
+:: Pause 
+scrcpy -s %ip%:5555
